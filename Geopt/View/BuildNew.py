f9a67a53caed6d9925b6c67360d2558643d3a001
Squidgeypea <sophie.turner@students.plymouth.ac.uk>
2020-10-23 22:29:34 +0100

    Made a loop which creates a clickable periodic table from XML data.

--- Geopt/View/BuildNew.py
+++ Geopt/View/BuildNew.py
@@ -1,9 +1,35 @@
 from View.Shared import *
 from Controller.BuildNew import *
 
+
+def MakeTable(frame, periods, groups, xmlList):
+    for i in range(periods):
+        for j in range(groups):
+            element = Button(frame, text=xmlList[i][j], command=TestClick, font=('Agency FB', 12))
+            element.grid(row=i, column=j)
+            element.config(width=5)
+
+
+def TestClick():
+    print("Clicked the button.")
+
+
+# get list from XML
+# just testing it with this for now...
+list = [("A", "", "", "B"),
+        ("C", "D", "E", "F"),
+        ("G", "H", "I", "J")]
+
+groups = len(list[0])
+periods = len(list)
+
+root = SetupWindow()
+
 tableFrame = Frame(root)
 tableFrame.pack()
 
+MakeTable(tableFrame, periods, groups, list)
 
+root.mainloop()
 
 
--- Geopt/View/Shared.py
+++ Geopt/View/Shared.py
@@ -1,17 +1,14 @@
 import tkinter as tk
 from tkinter import *
-from tkinter import ttk
 
-root = tk.Tk()
-root.title('Geopt')
-root.resizable(1, 1)
-
-bannerFrame = Frame(root, bg='#222222')
-bannerFrame.pack()
-
-lblName = Label(bannerFrame, text='Geopt', font=('Agency FB', 20), fg='limegreen', bg='#222222')
-lblName.pack(side=LEFT, padx=20)
-lblDescription = Label(bannerFrame, text='Molecular geometry optimisation', font=('Agency FB', 18), fg='white', bg='#222222')
-lblDescription.pack(pady=3)
-
-root.mainloop()
+def SetupWindow():
+    root = tk.Tk()
+    root.title('Geopt')
+    root.resizable(1, 1)
+    bannerFrame = Frame(root, bg='#222222')
+    bannerFrame.pack()
+    lblName = Label(bannerFrame, text='Geopt', font=('Agency FB', 20), fg='limegreen', bg='#222222')
+    lblName.pack(side=LEFT, padx=20)
+    lblDescription = Label(bannerFrame, text='Molecular geometry optimisation', font=('Agency FB', 18), fg='white', bg='#222222')
+    lblDescription.pack(pady=3)
+    return root
