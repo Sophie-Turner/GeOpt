3ed1a2c91d716ed21ecc5aa7a2941e5c900934aa
Squidgeypea <sophie.turner@students.plymouth.ac.uk>
2020-10-24 16:27:38 +0100

    Populated a clickable table with the first 36 elements from XML.

--- Geopt/Controller/BuildNew.py
+++ Geopt/Controller/BuildNew.py
@@ -1,1 +1,15 @@
 # Functions associated with the BuildNew View
+import xml.etree.ElementTree as ET
+
+def GetXML():
+    tree = ET.parse('../Model/elements.xml')
+    treeroot = tree.getroot()
+    return treeroot
+    # how to get certain XML elements:
+    #for element in treeroot:
+        #print(element.tag, element.attrib)
+        #print(element[2].text)
+    #print(root[0][2].text)
+    #print("len(treeroot):", len(treeroot))
+    #print("len(treeroot[0]):", len(treeroot[0]))
+
--- Geopt/View/BuildNew.py
+++ Geopt/View/BuildNew.py
@@ -2,11 +2,14 @@ from View.Shared import *
 from Controller.BuildNew import *
 
 
-def MakeTable(frame, periods, groups, xmlList):
+def MakeTable(frame, periods, xmlList):
     for i in range(periods):
-        for j in range(groups):
-            element = Button(frame, text=xmlList[i][j], command=TestClick, font=('Agency FB', 12))
-            element.grid(row=i, column=j)
+        for j in xmlList[i]:
+
+            symbol = j[0].text
+            group = j[4].text
+            element = Button(frame, text=symbol, command=TestClick, font=('Agency FB', 12))
+            element.grid(row=i, column=group)
             element.config(width=5)
 
 
@@ -15,20 +18,21 @@ def TestClick():
 
 
 # get list from XML
-# just testing it with this for now...
-list = [("A", "", "", "B"),
-        ("C", "D", "E", "F"),
-        ("G", "H", "I", "J")]
-
-groups = len(list[0])
-periods = len(list)
+list = GetXML()
 
 root = SetupWindow()
 
 tableFrame = Frame(root)
 tableFrame.pack()
 
-MakeTable(tableFrame, periods, groups, list)
+# Populate the main blocks
+periods = 4
+MakeTable(tableFrame, periods, list)
+
+# Populate the f block
+# We will need to change the way it finds them as this will find the first 2 periods atm, not the last 2
+#periods = 2
+#MakeTable(tableFrame, periods, list)
 
 root.mainloop()
 
