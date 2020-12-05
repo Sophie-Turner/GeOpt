6f1fc9cc28e9169de1baef48e51bd39cf784e4ff
Squidgeypea <sophie.turner@students.plymouth.ac.uk>
2020-12-05 18:44:38 +0000

    The user can view the optimised molecule.

--- Geopt/View/Analysis2.py
+++ Geopt/View/Analysis2.py
@@ -1,7 +1,7 @@
 from ase.io import write
-from PIL import Image, ImageTk
 from tkinter import *
 import tkinter
+from PIL import Image, ImageTk
 from Controller.Analysis import *
 from View.Shared import SetUpWindow
 
@@ -10,6 +10,9 @@ def StartAnalysis(elementsList):
     window = tk.Toplevel()
     SetUpWindow(window)
     molecule, energy = DoTheEA(elementsList)
+
+    write("Images/bestBuildUp.png", molecule, rotation='10x,30y,0z')
+
     picturesFrame = Frame(window)
 
     canvases = []
@@ -18,12 +21,14 @@ def StartAnalysis(elementsList):
     sizes = [200, 100, 100, 300]
     colspans = [2, 1, 1, 1]
     rowspans = [1, 1, 1, 3]
-    texts = ['Best configuration found', '#2', '#3', 'Potential energy surface']
+    texts = ['Best configuration found   {:.4f} eV'.format(energy),'#2   {:.4f} eV'.format(energy),'#3   {:.4f} eV'.format(energy), 'Potential energy surface']
+    imageBest = PhotoImage(file="Images/bestBuildUp.png")
 
     for i in range(4):
         size = sizes[i]
         canvas = tkinter.Canvas(picturesFrame, height=size, width=size)
         canvas.grid(row=rows[i], column=cols[i], rowspan=rowspans[i], columnspan=colspans[i], padx='10', pady='5')
+        canvas.create_image(size/2, size/2, image=imageBest)
         canvases.append(canvas)
 
     SetColours(picturesFrame)
